778c9cff0ae79cf14be59d2496d5a1bc97936f24
eyalk6 <100998935+eyalk6@users.noreply.github.com>
2022-03-09 17:52:59 +0200

    Add files via upload

---
+++ M2Eyal.py
@@ -0,0 +1,20 @@
+#A
+
+def reverse(sentence, reverse_word):
+ try:
+    new = sentence
+    if reverse_word in sentence:
+            word_len = len(reverse_word)
+            word_index = new.index(reverse_word)
+            part1 = new[:word_index]
+            part2 = new[word_len + word_index:]
+            h = reverse_word[::-1]
+            end = part1 + h + part2
+            return (end)
+    else:
+            return ("The word was not found")
+ except:
+     return "invalid input"
+
+reverse("I like apples and I also like bananas", "like")
+
