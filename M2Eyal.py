18ac4f13f067dbe01f44f1942a46f877459edd48
eyalk6 <100998935+eyalk6@users.noreply.github.com>
2022-03-09 18:09:47 +0200

    Add files via upload

--- M2Eyal.py
+++ M2Eyal.py
@@ -8,8 +8,8 @@ def reverse(sentence, reverse_word):
             word_index = new.index(reverse_word)
             part1 = new[:word_index]
             part2 = new[word_len + word_index:]
-            h = reverse_word[::-1]
-            end = part1 + h + part2
+            r = reverse_word[::-1]
+            end = part1 + r + part2
             return (end)
     else:
             return ("The word was not found")
